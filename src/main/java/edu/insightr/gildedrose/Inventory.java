9362c62e2c27d204085394234d122be0cccb1059
Xraigor <43266161+Xraigor@users.noreply.github.com>
2018-11-04 21:59:37 +0100

    Add files via upload

--- src/main/java/edu/insightr/gildedrose/Inventory.java
+++ src/main/java/edu/insightr/gildedrose/Inventory.java
@@ -1,9 +1,6 @@
 package edu.insightr.gildedrose;
 
-import java.awt.*;
-import java.util.Arrays;
-
-public class Inventory<items> {
+public class Inventory {
 
     public Item[] getItems() {
         return items;
@@ -33,11 +30,6 @@ public class Inventory<items> {
 
     }
 
-
-    // 1- Add to github and add a tag
-    // 2- Create a branch named "visitor"
-    // 3- use the visitor pattern
-
     public void printInventory() {
         System.out.println("***************");
         for (Item item : items) {
@@ -46,133 +38,34 @@ public class Inventory<items> {
         System.out.println("***************");
         System.out.println("\n");
     }
-    public void Visitor (){
-        for (int i = 0; i < items.length; i++) {
-            //visit +5 Dexterity Vest
-            if ("+5 Dexterity Vest".equals(items[i].getName())){
-                //SellIn >= 0
-                if (items[i].getSellIn() >= 0
-                        && items[i].getQuality() > 0){
-                    items[i].setQuality(items[i].getQuality() - 1);
-                }
-                //SellIn < 0
-                else if (items[i].getSellIn() < 0
-                        && items[i].getQuality() > 0){
-                    items[i].setQuality(items[i].getQuality() - 2);
-                }
-                items[i].setSellIn(items[i].getSellIn() - 1);
-                // check Quality
-                if (items[i].getQuality() <= 0){
-                    items[i].setQuality(0);
-                }
+    //new update Quality
+    public  void updateQuality() {
+        //use interface ItemStrategy to Encapsulate the different strategy
+        ItemStrategy itemStrategy;
+        for (Item item : items) {
+            switch (item.getName()) {
+                case "Aged Brie":
+                    itemStrategy = new AgedBrie();
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    itemStrategy = new BackstagePasses();
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    itemStrategy = new Sulfuras();
+                    break;
+                case "Conjured Mana Cake":
+                    itemStrategy = new Conjured();
+                    break;
+                default:
+                    itemStrategy = new NormalItem();
             }
-            //visit Aged Brie
-            if ("Aged Brie".equals(items[i].getName())) {
-                //SellIn >= 0
-                if (items[i].getSellIn() >= 0
-                        && items[i].getQuality() > 0
-                        && items[i].getQuality() < 50) {
-                    items[i].setQuality(items[i].getQuality() + 1);
-                }
-                //SellIn < 0
-                else if (items[i].getSellIn() < 0
-                        && items[i].getQuality() > 0
-                        && items[i].getQuality() < 50) {
-                    items[i].setQuality(items[i].getQuality() + 2);
-                }
-                    items[i].setSellIn(items[i].getSellIn() - 1);
-                // check Quality
-                if (items[i].getQuality() >= 50) {
-                    items[i].setQuality(50);
-                }
-                }
-
-            //visit Elixir of the Mongoose
-            if ("Elixir of the Mongoose".equals(items[i].getName())){
-            //SellIn > 0
-            if (items[i].getSellIn() >= 0
-                    && items[i].getQuality() > 0){
-                items[i].setQuality(items[i].getQuality() - 1);
-            }
-            //SellIn < 0
-            else if (items[i].getSellIn() < 0
-                    && items[i].getQuality() > 0){
-                items[i].setQuality(items[i].getQuality() - 2);
-            }
-            items[i].setSellIn(items[i].getSellIn() - 1);
-                // check Quality
-                if (items[i].getQuality() <= 0){
-                    items[i].setQuality(0);
-                }
-            }
-            //visit Sulfuras, Hand of Ragnaros
-            if ("Sulfuras, Hand of Ragnaros".equals(items[i].getName())){
-                items[i].setQuality(items[i].getQuality());
-                items[i].setSellIn(items[i].getSellIn());
-            }
-            //visit Backstage passes to a TAFKAL80ETC concert
-            if ("Backstage passes to a TAFKAL80ETC concert".equals(items[i].getName())){
-                if(items[i].getSellIn() <= 0){
-                    items[i].setQuality(0);
-                }
-                else if(items[i].getSellIn() >= 50){
-                    items[i].setQuality(items[i].getQuality());
-                }
-                //SellIn <= 5
-                else if(items[i].getSellIn() <= 5
-                        && items[i].getQuality() < 50){
-                    items[i].setQuality(items[i].getQuality() + 3);
-                }
-                //SellIn <= 10
-                else if(items[i].getSellIn() <= 10
-                        && items[i].getQuality() < 50){
-                    items[i].setQuality(items[i].getQuality() + 2);
-                }
-                //SellIn > 10
-                else if( items[i].getQuality() < 50){
-                    items[i].setQuality(items[i].getQuality() + 1);
-                }
-                items[i].setSellIn(items[i].getSellIn() - 1);
-                // check Quality
-                if (items[i].getQuality() >= 50) {
-                    items[i].setQuality(50);
-                }
-            }
-            //visit Conjured Mana Cake
-            if ("Conjured Mana Cake".equals(items[i].getName())){
-                //SellIn >= 0
-                if (items[i].getSellIn() >= 0
-                        && items[i].getQuality() > 0){
-                    items[i].setQuality(items[i].getQuality() - 2);
-                }
-                //SellIn < 0
-                else if (items[i].getSellIn() < 0
-                        && items[i].getQuality() > 0){
-                    items[i].setQuality(items[i].getQuality() - 4);
-                }
-                items[i].setSellIn(items[i].getSellIn() - 1);
-                // check Quality
-                if (items[i].getQuality() <= 0){
-                    items[i].setQuality(0);
-                }
-            }
-
+            itemStrategy.update(item);
         }
     }
-
-/*
-   public void superTruc(){
-        Visitor visitor = new Visitor();
-        for (int i = 0; 1 < items.length;i++){
-            items[i].accept(visitor);
-        }
-
-    }*/
-
     public static void main(String[] args) {
         Inventory inventory = new Inventory();
         for (int i = 0; i < 10; i++) {
-            inventory.Visitor();
+            inventory.updateQuality();
             inventory.printInventory();
         }
     }
--- src/main/java/edu/insightr/gildedrose/Item.java
+++ src/main/java/edu/insightr/gildedrose/Item.java
@@ -1,10 +1,10 @@
 package edu.insightr.gildedrose;
 
+
 public class Item {
 
     private String name;
     private int sellIn;
-
     private int quality;
 
     public Item(String name, int sellIn, int quality) {
