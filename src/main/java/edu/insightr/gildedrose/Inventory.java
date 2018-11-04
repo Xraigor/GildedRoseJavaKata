6e16f964fe4d10020a9e01eebcce22f74c1b8459
Xraigor <43266161+Xraigor@users.noreply.github.com>
2018-11-04 15:27:26 +0100

    Just change the method, not using visitor patterns.

--- src/main/java/edu/insightr/gildedrose/Inventory.java
+++ src/main/java/edu/insightr/gildedrose/Inventory.java
@@ -1,8 +1,9 @@
 package edu.insightr.gildedrose;
 
+import java.awt.*;
 import java.util.Arrays;
 
-public class Inventory {
+public class Inventory<items> {
 
     public Item[] getItems() {
         return items;
@@ -45,76 +46,125 @@ public class Inventory {
         System.out.println("***************");
         System.out.println("\n");
     }
-
-
-    public void updateQuality() {
+    public void Visitor (){
         for (int i = 0; i < items.length; i++) {
-            if (items[i].getName() != "Aged Brie"
-                    && items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].getQuality() > 0) {
-                    if (items[i].getName() != "Sulfuras, Hand of Ragnaros"
-                        && items[i].getName() != "Conjured Mana Cake") {
-                        items[i].setQuality(items[i].getQuality() - 1);
-                    }
-                    else if(items[i].getName() != "Sulfuras, Hand of Ragnaros"){
-                        items[i].setQuality(items[i].getQuality() - 2);
-                    }
+            //visit +5 Dexterity Vest
+            if ("+5 Dexterity Vest".equals(items[i].getName())){
+                //SellIn >= 0
+                if (items[i].getSellIn() >= 0
+                        && items[i].getQuality() > 0){
+                    items[i].setQuality(items[i].getQuality() - 1);
                 }
-            } else {
-                if (items[i].getQuality() < 50) {
-                    items[i].setQuality(items[i].getQuality() + 1);
-
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items[i].getName())) {
-                        if (items[i].getSellIn() < 11) {
-                            if (items[i].getQuality() < 50) {
-                                items[i].setQuality(items[i].getQuality() + 1);
-                            }
-                        }
-
-                        if (items[i].getSellIn() < 6) {
-                            if (items[i].getQuality() < 50) {
-                                items[i].setQuality(items[i].getQuality() + 1);
-                            }
-                        }
-                    }
+                //SellIn < 0
+                else if (items[i].getSellIn() < 0
+                        && items[i].getQuality() > 0){
+                    items[i].setQuality(items[i].getQuality() - 2);
                 }
-            }
-
-            if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
                 items[i].setSellIn(items[i].getSellIn() - 1);
-            }
-
-            if (items[i].getSellIn() < 0) {
-                if (items[i].getName() != "Aged Brie") {
-                    if (items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].getQuality() > 0) {
-                            if (items[i].getName() != "Sulfuras, Hand of Ragnaros"
-                                    && items[i].getName() != "Conjured Mana Cake") {
-                                items[i].setQuality(items[i].getQuality() - 1);
-                            }
-                            else if(items[i].getName() != "Sulfuras, Hand of Ragnaros"){
-                                items[i].setQuality(items[i].getQuality() - 2);
-                            }
-                        }
-                    } else {
-                        items[i].setQuality(items[i].getQuality() - items[i].getQuality());
-                    }
-                } else {
-                    if (items[i].getQuality() < 50) {
-                        items[i].setQuality(items[i].getQuality() + 1);
-                    }
+                // check Quality
+                if (items[i].getQuality() <= 0){
+                    items[i].setQuality(0);
                 }
             }
+            //visit Aged Brie
+            if ("Aged Brie".equals(items[i].getName())) {
+                //SellIn >= 0
+                if (items[i].getSellIn() >= 0
+                        && items[i].getQuality() > 0
+                        && items[i].getQuality() < 50) {
+                    items[i].setQuality(items[i].getQuality() + 1);
+                }
+                //SellIn < 0
+                else if (items[i].getSellIn() < 0
+                        && items[i].getQuality() > 0
+                        && items[i].getQuality() < 50) {
+                    items[i].setQuality(items[i].getQuality() + 2);
+                }
+                    items[i].setSellIn(items[i].getSellIn() - 1);
+                // check Quality
+                if (items[i].getQuality() >= 50) {
+                    items[i].setQuality(50);
+                }
+                }
+
+            //visit Elixir of the Mongoose
+            if ("Elixir of the Mongoose".equals(items[i].getName())){
+            //SellIn > 0
+            if (items[i].getSellIn() >= 0
+                    && items[i].getQuality() > 0){
+                items[i].setQuality(items[i].getQuality() - 1);
+            }
+            //SellIn < 0
+            else if (items[i].getSellIn() < 0
+                    && items[i].getQuality() > 0){
+                items[i].setQuality(items[i].getQuality() - 2);
+            }
+            items[i].setSellIn(items[i].getSellIn() - 1);
+                // check Quality
+                if (items[i].getQuality() <= 0){
+                    items[i].setQuality(0);
+                }
+            }
+            //visit Sulfuras, Hand of Ragnaros
+            if ("Sulfuras, Hand of Ragnaros".equals(items[i].getName())){
+                items[i].setQuality(items[i].getQuality());
+                items[i].setSellIn(items[i].getSellIn());
+            }
+            //visit Backstage passes to a TAFKAL80ETC concert
+            if ("Backstage passes to a TAFKAL80ETC concert".equals(items[i].getName())){
+                if(items[i].getSellIn() <= 0){
+                    items[i].setQuality(0);
+                }
+                else if(items[i].getSellIn() >= 50){
+                    items[i].setQuality(items[i].getQuality());
+                }
+                //SellIn <= 5
+                else if(items[i].getSellIn() <= 5
+                        && items[i].getQuality() < 50){
+                    items[i].setQuality(items[i].getQuality() + 3);
+                }
+                //SellIn <= 10
+                else if(items[i].getSellIn() <= 10
+                        && items[i].getQuality() < 50){
+                    items[i].setQuality(items[i].getQuality() + 2);
+                }
+                //SellIn > 10
+                else if( items[i].getQuality() < 50){
+                    items[i].setQuality(items[i].getQuality() + 1);
+                }
+                items[i].setSellIn(items[i].getSellIn() - 1);
+                // check Quality
+                if (items[i].getQuality() >= 50) {
+                    items[i].setQuality(50);
+                }
+            }
+            //visit Conjured Mana Cake
+            if ("Conjured Mana Cake".equals(items[i].getName())){
+                //SellIn >= 0
+                if (items[i].getSellIn() >= 0
+                        && items[i].getQuality() > 0){
+                    items[i].setQuality(items[i].getQuality() - 2);
+                }
+                //SellIn < 0
+                else if (items[i].getSellIn() < 0
+                        && items[i].getQuality() > 0){
+                    items[i].setQuality(items[i].getQuality() - 4);
+                }
+                items[i].setSellIn(items[i].getSellIn() - 1);
+                // check Quality
+                if (items[i].getQuality() <= 0){
+                    items[i].setQuality(0);
+                }
+            }
+
         }
     }
 
-
-   /* public void superTruc(){
-        Visitor visitorQuality = new VisitorQuality();
-        Visitor visitorSellIn = new VisitorSellIn();
+/*
+   public void superTruc(){
+        Visitor visitor = new Visitor();
         for (int i = 0; 1 < items.length;i++){
-            item.accept(visitorQuality);
-            item.accept(visitorSellIn);
+            items[i].accept(visitor);
         }
 
     }*/
@@ -122,7 +172,7 @@ public class Inventory {
     public static void main(String[] args) {
         Inventory inventory = new Inventory();
         for (int i = 0; i < 10; i++) {
-            inventory.updateQuality();
+            inventory.Visitor();
             inventory.printInventory();
         }
     }
